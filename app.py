c9dcac78ad4c33245aea7894a638af635f110be0
David <ddkpham@gmail.com>
2019-01-25 11:19:30 -0800

    add web scraping route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 #Initiate flaskapp 
 
@@ -8,9 +9,14 @@ app = Flask(__name__)
 def hello_word():
     return 'Hello, World'
 
-@app.route('/hello')
-def hello_word2():
-    return 'Hello, David'
+@app.route('/render')
+def temp_render():
+    return render_template('index.html')
+
+@app.route('/scrape')
+def web_scrape():
+    return 'scraping to be implemented soon!'
+
 
 if __name__=="__main__":
     app.run(debug=True)
